8ccb7648f520d73be8fa021b17a3f2880d1549b4
godzilla144 <kubokenta54@gmail.com>
2020-10-15 13:06:23 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -5,12 +5,12 @@ function setup() {
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(60, 100, 120);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  fill(94, 55, 43);
+  triangle(0,120,106,108,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
-  text("46", 68, 100);
+  text("40", 68, 100);
 }
