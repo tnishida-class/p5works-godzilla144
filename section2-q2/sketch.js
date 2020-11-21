1fbcbbc7637ca51692eeb348a4fe8a6e4c6fc81c
godzilla144 <kubokenta54@gmail.com>
2020-11-22 01:12:18 +0900

    section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,14 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+    let k = i + j;
+    if(k % 2 == 0){
+      fill(255);
+    }
+    else if (k % 2 == 1){
+      fill (200);
+    }
+    rect(size * i, size * j, size, size);
     }
   }
 }
