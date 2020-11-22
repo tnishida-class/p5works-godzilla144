9048105cef6edb20a43882e1631673cb05528ace
godzilla144 <kubokenta54@gmail.com>
2020-11-22 15:33:02 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love Kenshi Yonedzu", 100, 255);
 }
 
-function balloon(t){
+function balloon(t, cl, txcl){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
+  fill(cl);
   rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+  fill(txcl);
   text(t, p, h + p);
+  fill(cl);
+  triangle(w + p, h + p, w + p - 10, h + p, w + p, h + p + 10);
 }
