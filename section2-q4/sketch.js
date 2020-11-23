af23c8ab097d44f43b3c69cfe408f431055ba65b
godzilla144 <kubokenta54@gmail.com>
2020-11-23 17:00:45 +0900

    Change section 2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,31 @@
 // ギリシャ国旗
-function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
-  // noStroke();
-  background(255);
+// function setup() {
+//   const blue = color(0, 51, 160);
+//   createCanvas(270, 180);
+//   // noStroke();
+//   background(255);
+//
+//   let d = height / 9; // 縞1本の太さ
+//
+//   for(let i = 0; i < 9; i++){
+//     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+//     rect(0, i * d, width, (i + 1) * d);
+//     if(i %
+//     })
+//   }
+//
+//   fill(blue);
+//   let size = d * 5;
+//   rect(0, 0, size, size);
+//
+//   fill(255);
+//   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+// }
+function setup(){
+  createCanvas(500, 300);
+  noStroke();
+  background('green');
 
-  let d = height / 9; // 縞1本の太さ
-
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
-
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
-
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  fill(255, 0, 0);
+  ellipse(220, 150, 200);
 }
