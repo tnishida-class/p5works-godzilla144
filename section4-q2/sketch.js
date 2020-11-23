0087cefb9d0d0b2f1f230cb4ff08f17e7921aa12
godzilla144 <kubokenta54@gmail.com>
2020-11-23 16:36:16 +0900

    Changed section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,17 +1,23 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy, angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 4;
+  vy = 4;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+  fill('yellow');
+  angle += 0.1;
+  x += vx;
+  y += vy;
+  star(x, y, 30, angle);
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -25,6 +31,7 @@ function star(cx, cy, r, angle){
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+    theta += angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
